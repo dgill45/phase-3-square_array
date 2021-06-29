d12868e7f4c693b0a4c29da90ecb422c62eef646
donellgill <donell.gill@outlook.com>
2021-06-29 07:59:10 -0400

    done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  
+  numbers = [2,4,6,8]
+  count =0
+  numbers.each do |number|
+    squares = number * number
+  square_array(squares)
+
+end
+
+def square_array(mo_numbers)
+  mo_numbers = [1, 2, 3, 4]
+  puts "collect mo_numbers : #{mo_numbers.collect {|mo_number| mo_number * mo_number}}"
 end
